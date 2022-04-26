cb713ca331431137bc5ecd18412a8fda17c1e090
Alciomar Hollanda <alciomarhollanda@gmail.com>
2022-04-26 20:24:06 -0300

    Introdução ao PHP

---
+++ 09-introPhp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$txt = "PHP";
+$n1 = 1;
+
+echo "$n1 - My first $txt script! - Alciomar";
+echo "<br>";
+echo "I love " . $txt . "!";
+
+echo "<br>";
+
+$x = 5;
+$y = 10;
+echo $x + $y;
+
+?>
+
+</body>
+</html>
